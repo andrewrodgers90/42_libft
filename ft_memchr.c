728dfd7d77cb69ff3da21b2adeeabcc08f200b65
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-20 00:37:10 +0700

    Update ft_memchr.c
    Remove brackets

--- ft_memchr.c
+++ ft_memchr.c
@@ -20,9 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (*(unsigned char *)(s + i) == (unsigned char)c)
-		{
 			return ((void *)(s + i));
-		}
 		i++;
 	}
 	return (0);
