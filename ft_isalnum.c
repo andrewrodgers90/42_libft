0ecf5afc7b1240e0ebc093ab1f022ca36e826df4
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-19 23:17:15 +0700

    Update ft_isalnum.c
    Remove brackets

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -13,19 +13,11 @@
 int	ft_isalnum(int c)
 {
 	if (c >= '0' && c <= '9')
-	{
 		return (1);
-	}
 	else if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
 	else if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
