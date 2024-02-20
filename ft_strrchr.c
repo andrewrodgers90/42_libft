3854e6c3bb9e9e8535ce85363fbaa092f50af0b8
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-21 00:08:29 +0700

    Update ft_strrchr.c
    Remove brackets.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -20,22 +20,16 @@ char	*ft_strrchr(const char *s, int c)
 	if ((unsigned char)c == '\0')
 	{
 		while (s[i] != '\0')
-		{
 			i++;
-		}
 		return ((char *)(s + i));
 	}
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	i -= 1;
 	while (i >= 0)
 	{
 		if (s[i] == (unsigned char)c)
-		{
 			return ((char *)(s + i));
-		}
 		i--;
 	}
 	return (0);
